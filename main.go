a9611f736e6d0c915bea3d6dd3844f030f48f22f
Norbert <wagner.norbert277@outlook.com>
2021-02-11 19:57:10 +0100

    code clean up and added single file support

--- main.go
+++ main.go
@@ -40,12 +40,27 @@ func execMOD() {
 	cmd.Run()
 }
 
+func execFile(filepath string) {
+	cmd := exec.Command("go", "run", filepath)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Run()
+}
+
+func timeLog() {
+	log := time.Now().Format("2006-01-02, 15:04 \n\n")
+	log = `Building project: ` + log + `Program result: `
+	cmd := exec.Command("printf", "\\e[1;34m%-6s\\e[m\n", log)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Run()
+}
+
 func main() {
 	doneChan := make(chan bool)
 	filepath := os.Args[1]
 	modOrFile := os.Args[2]
 	fmt.Println(filepath)
-	var log string
 	for true {
 		go func(doneChan chan bool) {
 			defer func() {
@@ -58,22 +73,17 @@ func main() {
 			}
 
 			fmt.Println("File has been changed")
-			os.Chdir(filepath)
-
 			if modOrFile == "mod" {
-				log = time.Now().Format("2006-01-02, 15:04 \n\n")
-				log = `Building project: ` + log + `Program result: `
-				cmd := exec.Command("printf", "\\e[1;34m%-6s\\e[m\n", log)
-				cmd.Stdout = os.Stdout
-				cmd.Stderr = os.Stderr
-				cmd.Run()
-				cmd = exec.Command("go", "build", "-o", "app-godemon-app-godemon-tmp-generated")
+				os.Chdir(filepath)
+				timeLog()
+				cmd := exec.Command("go", "build", "-o", "app-godemon-app-godemon-tmp-generated")
 				cmd.Stdout = os.Stdout
 				cmd.Stderr = os.Stderr
 				cmd.Run()
 				go execMOD()
 			} else if modOrFile == "file" {
-
+				timeLog()
+				go execFile(filepath)
 			}
 		}(doneChan)
 		<-doneChan
