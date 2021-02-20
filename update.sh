89a9dff84a1ad8122804fdb0c639016e4b6e9022
nwagner <wagner.norbert277@outlook.com>
2021-02-21 00:57:43 +0100

    added update.sh and installer.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 wget https://github.com/nProgrammer/godemon/releases/download/2.5.5/godemon
-sudo cp -r godemon /bin/godemon
+sudo rm -r /bin/godemon
+sudo mv godemon /bin/godemon
 sudo chmod 777 /bin/godemon
