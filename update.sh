b22e17b0d44da72a3a49f84ad6d6338241a568d7
nwagner <wagner.norbert277@outlook.com>
2021-02-21 01:04:10 +0100

    scripts updated

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-wget https://github.com/nProgrammer/godemon/releases/download/2.5.5/godemon
+wget https://github.com/nProgrammer/godemon/releases/download/2.5.6/godemon
 sudo rm -r /bin/godemon
 sudo mv godemon /bin/godemon
 sudo chmod 777 /bin/godemon
